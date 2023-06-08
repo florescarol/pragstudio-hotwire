48165830fc5b1de45e2e1c30dcff8e9ded5583f4
Carol <fflorescarol@terra.com.br>
2023-06-07 21:01:00 -0300

    11 - using turbo streams on show fish catches

--- app/controllers/fish_catches_controller.rb
+++ app/controllers/fish_catches_controller.rb
@@ -29,7 +29,10 @@ class FishCatchesController < ApplicationController
     @fish_catch = current_user.fish_catches.new(fish_catch_params)
 
     if @fish_catch.save
-      redirect_to tackle_box_item_for_catch(@fish_catch)
+      @fish_catches = fish_catches_for_bait(@fish_catch.bait)
+      @new_catch = current_user.fish_catches.new(bait: @fish_catch.bait)
+
+      render :update
     else
       render :new, status: :unprocessable_entity
     end
